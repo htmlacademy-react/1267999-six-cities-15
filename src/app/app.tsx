9ad27931eb102ea9baa83eb0aa282650a62b0f12
Konstanin <lable_01@mail.ru>
2024-03-04 22:48:47 +0300

    обновил роутер до 6.22, удалил className, добавил clsx, накатил роутер

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,11 +1,30 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import MainPage from 'pages/main-page';
+import { Path } from '../const';
+import LoginPage from 'pages/login-page';
+import NotFound from 'pages/not-found';
 
 type TAppMainProps = {
   cardsCount: number;
 };
 
 function App({ cardsCount }: TAppMainProps) {
-  return <MainPage cardsCount={cardsCount} />;
+  const router = createBrowserRouter([
+    {
+      path: Path.Main,
+      element: <MainPage cardsCount={cardsCount} />,
+    },
+    {
+      path: Path.Login,
+      element: <LoginPage />,
+    },
+    {
+      path: Path.NotFound,
+      element: <NotFound />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,11 @@
 export const Settings = {
   CardsCount: 6,
 } as const;
+
+export const Path = {
+  Main: '/',
+  Login: '/Login',
+  Favorites: '/Favorites',
+  Offer: '/offer/:id',
+  NotFound: '*',
+} as const;
